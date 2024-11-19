be29636d66656cfabb85d264bb1ba914183fc631
tjex <tjex@tjex.net>
2024-11-19 15:55:18 +0100

    dotf batch update

--- lua/tjex/plugins/harpoon.lua
+++ lua/tjex/plugins/harpoon.lua
@@ -6,9 +6,9 @@ return {
 		local mark = require("harpoon.mark")
 
 		require("harpoon").setup({
-			menu = {
-				width = vim.api.nvim_win_get_width(0) - 200,
-			},
+			-- menu = {
+			-- 	width = vim.api.nvim_win_get_width(0) - 200,
+			-- },
 		})
 
 		local ui = require("harpoon.ui")
